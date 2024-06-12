b6d75c36b69796d0357422ec7e981b7f1cd0e511
hvbhanot17 <hvbhanot1@gmail.com>
2024-06-12 05:05:38 -0500

    V3

--- routes/eventsOps.go
+++ routes/eventsOps.go
@@ -8,6 +8,12 @@ import (
 )
 
 func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later."})
+		return
+	}
+	context.JSON(http.StatusOK, events)
 }
 
 func getEvent(context *gin.Context) {
